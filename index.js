cf9ca38280146529fa1c83c18381c4b354146564
wilian <wilian_vintelinks@hotmail.com>
2025-06-14 18:25:29 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ function gerarFaturaStr (fatura, pecas) {
                             minimumFractionDigits: 2 }).format;
   
     for (let apre of fatura.apresentacoes) {
-      const peca = pecas[apre.id];
-      let total = calcularTotalApresentacao(apre, peca);
+      //const peca = getPeca(apre);
+      let total = calcularTotalApresentacao(apre);
   
       // créditos para próximas contratações
       creditos += Math.max(apre.audiencia - 30, 0);
-      if (peca.tipo === "comedia") 
+      if (getPeca(apre).tipo === "comedia") 
          creditos += Math.floor(apre.audiencia / 5);
   
       // mais uma linha da fatura
-      faturaStr += `  ${peca.nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
+      faturaStr += `  ${getPeca(apre).nome}: ${formato(total/100)} (${apre.audiencia} assentos)\n`;
       totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura/100)}\n`;
@@ -26,16 +26,20 @@ function gerarFaturaStr (fatura, pecas) {
     return faturaStr;
   }
 
-  function calcularTotalApresentacao (apre, peca){
+  function getPeca(apresentacao){
+    return pecas[apresentacao.id];
+  }
+
+  function calcularTotalApresentacao (apre){
   let total = 0;
-  if (peca.tipo == "tragedia"){
+  if (getPeca(apre).tipo == "tragedia"){
     total = 40000;
     if(apre.audiencia > 30){
       total += 1000 * (apre.audiencia - 30);
     }
     return total;
   }
-  if (peca.tipo == "comedia"){
+  if (getPeca(apre).tipo == "comedia"){
     total = 30000;
     if(apre.audiencia > 20){
      total += 10000 + 500 * (apre.audiencia - 20);
@@ -43,7 +47,7 @@ function gerarFaturaStr (fatura, pecas) {
       total += 300 * apre.audiencia;
       return total;
     }
-  throw new Error(`Peça desconhecia: ${peca.tipo}`);
+  throw new Error(`Peça desconhecia: ${getPeca(apre).tipo}`);
   
 }
 
